b255e388ad6ca7f978f3c619a3a74ed2c9418e91
davidnjau <davidnjau21@gmail.com>
2023-09-05 08:56:39 +0300

    first commit

---
+++ app/src/main/java/com/dave/krom/FragmentUpload.kt
@@ -0,0 +1,11 @@
+package com.dave.krom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentUpload : Fragment(R.layout.fragment_upload) {
+
+}
---
+++ app/src/main/java/com/dave/krom/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.dave.krom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment=FragmentHome()
+        val secondFragment=FragmentUpload()
+
+        setCurrentFragment(firstFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(firstFragment)
+                R.id.upload->setCurrentFragment(secondFragment)
+
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+    }
+}
